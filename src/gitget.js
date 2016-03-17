82c8bb6662aaa71c0c9fcc75fe9cb3054e4d916b
James Quinlan <quinlanjc@vcu.edu>
2016-03-17 14:56:48 -0400

    refactoring to cater towards user-written promises

--- src/gitget.js
+++ src/gitget.js
@@ -18,44 +18,29 @@ class Profile {
         this.username = username;
     }
 
-    setHtml(r, e){
-        document.getElementById(e).innerText = r;
-    }
-
-    get(attr, e){
+    get(attr){
         let instance = this;
 
-        if( !instance.e ){
-            console.log('There is no e.');
-
-            fetch('https://api.github.com/users/' + this.username )
-                .then(response => response.json())
-                .then(function(profile){
-                    instance.answer = profile[attr];
-                    instance.set();
-                });
-
-            return instance;
-        } else {
-            console.log('Ready!');
-
-            fetch('https://api.github.com/users/' + this.username )
-                .then(response => response.json())
-                .then(function(profile){
-                    instance.answer = profile[attr];
-                });
-        }
-
+        return fetch('https://api.github.com/users/' + this.username )
+            .then(response => response.json())
+            .then(function(profile){
+                instance.answer = profile[attr];
+        });
     }
-    set(e){
-        let instance =  this;
-        if( !instance.answer ){
-            console.log('Not Ready Yet.')
-        } else {
-            document.getElementById(e).innerText = instance.answer;
-            console.log('Set!');
-        }
+
+    set(){
+        console.log(this.answer + ' blah');
     }
+
+    // set(e){
+    //     let instance =  this;
+    //     if( !instance.answer ){
+    //         console.log('Not Ready Yet.')
+    //     } else {
+    //         document.getElementById(e).innerText = instance.answer;
+    //         console.log('Set!');
+    //     }
+    // }
 }
 class Events {
     constructor(username){
